4409f1d2c7eead86b3a84a16c0c38ee35c7ea618
Logeodev <leo.logel@outlook.fr>
2025-05-16 12:22:32 +0200

    feat (mock service)

---
+++ MAUI-Sample/MAUI-Sample/Services/ApiMockSetup.cs
@@ -0,0 +1,141 @@
+﻿using System.Net;
+using System.Text.Json;
+using WireMock.RequestBuilders;
+using WireMock.ResponseBuilders;
+using WireMock.Server;
+using MAUI_Sample.Mock;
+using WireMock.Models;
+using WireMock.Settings;
+using IResponseProvider = WireMock.ResponseProviders.IResponseProvider;
+using TaskStatus = MAUI_Sample.Models.TaskStatus;
+using TaskInput = MAUI_Sample.Models.TaskInput;
+using WireMock;
+using GraphQL;
+
+namespace MAUI_Sample.Services
+{
+    public static class ApiMockSetup
+    {
+        public static WireMockServer ConfigureApiMock()
+        {
+            var mockServer = WireMockServer.Start();
+
+            // GET all tasks
+            mockServer
+                .Given(Request.Create().WithPath("/api/tasks").UsingGet())
+                .ThenRespondWith((req) => req
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyAsJson((req) => (TaskApiMockResponses.GetSampleTasks())));
+
+            // GET task by ID
+            mockServer
+                .Given(Request.Create().WithPath("/api/tasks/*").UsingGet())
+                .ThenRespondWith((builder) => builder
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyAsJson(req => (TaskApiMockResponses.GetTaskById(int.Parse(req.PathSegments[2])))));
+
+            // POST create task
+            mockServer
+                .Given(Request.Create().WithPath("/api/tasks").UsingPost())
+                .ThenRespondWith((builder) => builder
+                    .WithStatusCode(HttpStatusCode.Created)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyAsJson(req => (CreateTaskResponse(req))));
+
+            // PUT update task
+            mockServer
+                .Given(Request.Create().WithPath("/api/tasks/*").UsingPut())
+                .ThenRespondWith((builder) => builder
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyAsJson(req => (UpdateTaskResponse(req)))
+                );
+
+            // PUT update task status
+            mockServer
+                .Given(Request.Create().WithPath("/api/tasks/*/status").UsingPut())
+                .ThenRespondWith((builder) => builder
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyAsJson(req => (UpdateTaskStatusResponse(req)))
+                );
+
+            // DELETE task
+            mockServer
+                .Given(Request.Create().WithPath("/api/tasks/*").UsingDelete())
+                .ThenRespondWith((builder) => builder
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyAsJson(req => (DeleteTaskResponse(req)))
+                );
+
+            return mockServer;
+        }
+
+        private static object CreateTaskResponse(IRequestMessage req)
+        {
+            var body = req.Body;
+            var taskInput = JsonSerializer.Deserialize<TaskInput>(body);
+            return TaskApiMockResponses.CreateTask(taskInput);
+        }
+
+        private static IResponseProvider UpdateTaskResponse(IRequestMessage req)
+        {
+            var id = int.Parse(req.PathSegments[2]);
+            var taskInput = JsonSerializer.Deserialize<TaskInput>(req.Body);
+            var task = TaskApiMockResponses.UpdateTask(id, taskInput);
+
+            if (task != null)
+            {
+                return Response.Create()
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyAsJson(task);
+            }
+            else
+            {
+                return Response.Create()
+                    .WithStatusCode(HttpStatusCode.NotFound);
+            }
+        }
+
+        private static IResponseProvider UpdateTaskStatusResponse(IRequestMessage req)
+        {
+            var id = int.Parse(req.PathSegments[2]);
+            var statusObj = JsonSerializer.Deserialize<JsonElement>(req.Body);
+            var status = Enum.Parse<TaskStatus>(statusObj.GetProperty("status").GetString());
+            var task = TaskApiMockResponses.UpdateTaskStatus(id, status);
+
+            if (task != null)
+            {
+                return Response.Create()
+                    .WithStatusCode(HttpStatusCode.OK)
+                    .WithHeader("Content-Type", "application/json")
+                    .WithBodyAsJson(task);
+            }
+            else
+            {
+                return Response.Create()
+                    .WithStatusCode(HttpStatusCode.NotFound);
+            }
+        }
+
+        private static IResponseProvider DeleteTaskResponse(IRequestMessage req)
+        {
+            var id = int.Parse(req.PathSegments[2]);
+            var success = TaskApiMockResponses.DeleteTask(id);
+
+            if (success)
+            {
+                return Response.Create()
+                    .WithStatusCode(HttpStatusCode.NoContent);
+            }
+            else
+            {
+                return Response.Create()
+                    .WithStatusCode(HttpStatusCode.NotFound);
+            }
+        }
+    }
+}
---
+++ MAUI-Sample/MAUI-Sample/Services/MockTaskService.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http.Json;
+using MAUI_Sample.Models;
+using NJsonSchema.Extensions;
+using TaskStatus = MAUI_Sample.Models.TaskStatus;
+
+namespace MAUI_Sample.Services
+{
+    public class MockTaskService : ITaskService
+    {
+        private readonly HttpClient _httpClient;
+
+        public MockTaskService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<Models.Task>> GetTasksAsync(TaskStatus? status = null)
+        {
+            string url = "/api/tasks";
+            if (status.HasValue)
+            {
+                url += $"?status={status.Value}";
+            }
+            return await _httpClient.GetFromJsonAsync<IEnumerable<Models.Task>>(url);
+        }
+
+        public async Task<Models.Task> GetTaskByIdAsync(int id)
+        {
+            return await _httpClient.GetFromJsonAsync<Models.Task>($"/api/tasks/{id}");
+        }
+
+        public async Task<Models.Task> CreateTaskAsync(TaskInput taskInput)
+        {
+            var response = await _httpClient.PostAsJsonAsync("/api/tasks", taskInput);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<Models.Task>();
+        }
+
+        public async Task<Models.Task> UpdateTaskAsync(int id, TaskInput taskInput)
+        {
+            var response = await _httpClient.PutAsJsonAsync($"/api/tasks/{id}", taskInput);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<Models.Task>();
+        }
+
+        public async Task<Models.Task> UpdateTaskStatusAsync(int id, TaskStatus status)
+        {
+            var statusUpdate = new { status = status.ToString() };
+            var response = await _httpClient.PutAsJsonAsync($"/api/tasks/{id}/status", statusUpdate);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<Models.Task>();
+        }
+
+        public async System.Threading.Tasks.Task DeleteTaskAsync(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"/api/tasks/{id}");
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
